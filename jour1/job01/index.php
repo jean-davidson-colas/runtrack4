de3327f73a3233bbcf40e367db429622712c5280
colas <jean-davidson.colas@laplateforme.io>
2020-05-05 09:02:54 +0200

    Terminer manque joB04

---
+++ jour1/job01/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Introduction Media Queries</title>
+
+  <link rel="stylesheet"href="css/style1.css" />
+  <link rel="stylesheet" media="screen and (min-width: 1680px) and (max-width: 1920px)" href="css/style2.css" />
+  <link rel="stylesheet" media="screen and (min-width: 1280px) and (max-width: 1680px)" href="css/style3.css" />
+  <link rel="stylesheet" media="screen and (max-width: 1280px)" href="css/style4.css" /> 
+  <script src="script.js"></script>
+</head>
+<body>
+  ...
+  
+  ...
+</body>
+</html>
+
